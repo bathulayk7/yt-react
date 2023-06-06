8b8b2c4eb8d11185a298723ff9f113c3405e1f69
bathulayedukondalu <bathulayedukondalu@gmail.com>
2023-06-06 22:00:53 +0530

    multi select updated

--- src/components/dropdwonMultiSelect/MultiSelectCompReact.js
+++ src/components/dropdwonMultiSelect/MultiSelectCompReact.js
@@ -5,19 +5,25 @@ const countries = [
   { value: 1, label: "India" },
   { value: 2, label: "US" },
   { value: 3, label: "UK" },
+  { value: 4, label: "JAPAN" },
+  { value: 5, label: "RUSSIA" },
+  { value: 6, label: "CHINA" },
 ];
 
 const MultiSelectCompReact = () => {
   const [selected, setSelected] = useState([]);
 
   const handleOptions = (list) => {
-    setSelected(Array.isArray(list) ? list.map((item) => item.label) : []);
+    setSelected(list);
   };
+
+  let backEndList = selected && selected.map((list) => list.value);
+  console.log("backEndList", backEndList);
 
   return (
     <div>
-      <h2>{selected + ""}</h2>
-      <div style={{width:"200px"}} >
+      <h2>{backEndList.map(val=>val+",")}</h2>
+      <div style={{ width: "200px" }}>
         <Select options={countries} isMulti onChange={handleOptions} />
       </div>
     </div>
